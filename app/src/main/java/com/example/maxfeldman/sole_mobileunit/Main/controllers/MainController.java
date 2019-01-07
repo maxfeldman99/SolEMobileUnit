ace17aac869912b9a415fc8ec7fd7a4983d85001
MAX FELDMAN <maxfeldman99@gmail.com>
2019-01-07 10:35:35 +0200

    MainController added

--- app/src/main/java/com/example/maxfeldman/sole_mobileunit/Main/controllers/MainController.java
+++ app/src/main/java/com/example/maxfeldman/sole_mobileunit/Main/controllers/MainController.java
@@ -1,9 +1,37 @@
 package com.example.maxfeldman.sole_mobileunit.Main.controllers;
 
+import com.example.maxfeldman.sole_mobileunit.Main.Helpers.MongoDB;
+import com.example.maxfeldman.sole_mobileunit.Main.Server.Server;
+
 /**
  * Created by MAX FELDMAN on 07/01/2019.
  */
 
 public class MainController {
+
+    private Server serverController = new Server();
+    //private VideoController videoController = new VideoController();
+    private MongoDB mongoDB = new MongoDB();
+    private NetworkController networkController;
+
+
+
+    public MainController() {
+
+    }
+
+    private static MainController instance = null;
+
+    public static MainController getInstance() {
+        if (instance == null) {
+            instance = new MainController();
+        }
+        return instance;
+    }
+
+
+
+
+
 
 }
